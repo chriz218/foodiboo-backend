edf7fee58b7c71b1104a422dea745c6d9957c575
Christopher Thong <christhong123@gmail.com>
2020-02-14 14:51:24 +0800

    changed to decimal field

--- models/food.py
+++ models/food.py
@@ -4,6 +4,6 @@ import peewee as pw
 class Food(BaseModel):
     name = pw.CharField(unique=False)
     geolocation = pw.CharField(unique=False, null=True)
-    longitude = pw.CharField(null=True)
-    latitude = pw.CharField(null=True)
+    longitude = pw.DecimalField(null=True)
+    latitude = pw.DecimalField(null=True)
     price = pw.DecimalField(decimal_places=2, null=True)
