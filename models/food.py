06845e1955f93b72b61c2266d1d4a6f25a551519
Christopher Thong <christhong123@gmail.com>
2020-02-14 16:06:57 +0800

    attempt at fixing heroku again

--- models/food.py
+++ models/food.py
@@ -4,6 +4,6 @@ import peewee as pw
 class Food(BaseModel):
     name = pw.CharField(unique=False)
     # geolocation = pw.CharField(unique=False, null=True)
-    # longitude = pw.FloatField(null=True)
-    # latitude = pw.FloatField(null=True)
+    longitude = pw.FloatField(null=True)
+    latitude = pw.FloatField(null=True)
     price = pw.DecimalField(decimal_places=2, null=True)
